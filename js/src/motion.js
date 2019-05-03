531aaac37fcd37486a033979f2cf5967b2554e58
enqufun <enqufun@gmail.com>
2019-05-03 00:01:38 -0700

    Site updated: 2019-05-03 00:01:37

--- js/src/motion.js
+++ js/src/motion.js
@@ -77,7 +77,9 @@ $(document).ready(function () {
   sidebarToggleLines.push(sidebarToggleLine2nd);
   sidebarToggleLines.push(sidebarToggleLine3rd);
 
-  var SIDEBAR_WIDTH = '320px';
+  //var SIDEBAR_WIDTH = '320px';
+  var SIDEBAR_WIDTH = '20vw';
+
   var SIDEBAR_DISPLAY_DURATION = 200;
   var xPos, yPos;
 
